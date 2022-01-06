561aa0f461d80c5dc09e4fd7e139aba94f7775c9
liu <shellingfordly@qq.com>
2022-01-06 14:58:43 +0800

    feat: add css demo

---
+++ vue/vue3-source-code-learn/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { reactive, ref } from "vue";
+
+const obj = {
+  a: 1,
+  b: 2,
+};
+
+const proxy1 = reactive(obj);
+const proxy2 = reactive(obj);
+
+console.log(proxy1 == proxy2);
+console.log(proxy1 === proxy2);
+
+const data = [
+  {
+    id: 1,
+    value: "aaa",
+  },
+  {
+    id: 2,
+    value: "bbb",
+  },
+  {
+    id: 3,
+    value: "ccc",
+  },
+];
+
+const allData = ref([]);
+const showData = ref([]);
+
+allData.value = data;
+showData.value = data;
+
+console.log("---------");
+console.log(allData.value == showData.value);
+console.log(allData.value === showData.value);
+
+showData.value = allData.value.filter((v) => v.id < 2);
+
+console.log(showData.value);
+console.log(allData.value[0] === showData.value[0]);
+</script>
+
+<template></template>
---
+++ vue/vue3-source-code-learn/src/components/Proxy.vue
@@ -0,0 +1,16 @@
+<script setup>
+const data = {
+  a: 1,
+  b: 2,
+};
+
+const proxy = new Proxy(data, {
+  get(target, key, reciver) {
+    console.log("target", target);
+    console.log("key", key);
+    console.log("reciver", reciver);
+    return target;
+  },
+});
+console.log(proxy.a);
+</script>
